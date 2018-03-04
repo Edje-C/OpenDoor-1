9ec76d1061cc252a35f141d2754db0e107d5b607
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-03-04 01:29:09 -0500

    Added Google Maps

--- open_door/src/Components/Search/Map.js
+++ open_door/src/Components/Search/Map.js
@@ -1,35 +1,41 @@
 import React, { Component } from "react";
-import { Map, InfoWindow, Marker, GoogleApiWrapper, google } from "google-maps-react";
-import {Redirect} from 'react-router'
+import {
+  Map,
+  InfoWindow,
+  Marker,
+  GoogleApiWrapper,
+  google
+} from "google-maps-react";
+import { Redirect } from "react-router";
 export class MapContainer extends Component {
-    constructor(){
-        super()
-        this.state = {
-            selected: false,
-            buildingid: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      selected: false,
+      buildingid: ""
+    };
+  }
 
-        onMapClicked =(id)=> {
-           this.setState({
-               selected: true,
-               buildingid: id
-           })
-        }
+  onMapClicked = id => {
+    this.setState({
+      selected: true,
+      buildingid: id
+    });
+  };
   render() {
-      const {selected, buildingid} = this.state
+    const { selected, buildingid } = this.state;
     const style = {
       width: "700px",
       height: "300px"
     };
 
-    if(selected){
-        return <Redirect to={`/building/${buildingid}`} />;
-        this.setState({
-            selected: false
-        })
+    if (selected) {
+      return <Redirect to={`/building/${buildingid}`} />;
+      this.setState({
+        selected: false
+      });
     }
-   
+
     const { uniqueBuildingsArr } = this.props;
     return (
       <div id="map">
@@ -38,19 +44,18 @@ export class MapContainer extends Component {
           style={style}
           initialCenter={{
             lat: 40.7128,
-            lng: -73.935242 
+            lng: -73.935242
           }}
           zoom={11}
         >
-         {uniqueBuildingsArr.map(building => (
-             <Marker
-             title={'The marker`s title will appear as a tooltip.'}
-             name={'SOMA'}
-             position={{lat:building.latitude , lng: building.longitude }} 
-             onClick={() => this.onMapClicked(building.buildingid)}
-             >
-             </Marker>
-         ))}
+          {uniqueBuildingsArr.map(building => (
+            <Marker
+              title={"The marker`s title will appear as a tooltip."}
+              name={"SOMA"}
+              position={{ lat: building.latitude, lng: building.longitude }}
+              onClick={() => this.onMapClicked(building.buildingid)}
+            />
+          ))}
         </Map>
       </div>
     );
